bd2bb6ddaaaa3d3ec9048c1a62061585f080c00d
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-10-15 10:52:00 +0530

    first triangle?

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,44 @@
-#define GL_SILENCE_DEPRECATION
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
 
 int main(void)
 {
     GLFWwindow* window;
+    if (!glfwInit()) return -1; // initialize glfw
 
-    /* Initialize the library */
-    if (!glfwInit()) return -1;
+    window = glfwCreateWindow(1000, 1000, "renegl", NULL, NULL);
 
-    if (!window)
-    {
+    if (!window) {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(window); // create opengl rendering context
 
-    if(glewInit() != GLEW_OK) return -1;
+    if(glewInit() != GLEW_OK) return -1; // intialize glew
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    GLfloat vertex_buffer_data[] = { -.5f, -.5f, 0.0f,
+                                     0.5f, -.5f, 0.0f,
+                                     0.0f,  .5f, 0.0f,
+                                   };
+
+    GLuint vertextbuffer;
+    glGenBuffers(1, &vertextbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertextbuffer);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertex_buffer_data),vertex_buffer_data,GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0,3,GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT)*3, 0);
+
+    while (!glfwWindowShouldClose(window) && !glfwGetKey(window,GLFW_KEY_ESCAPE))
     {
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        /* Poll for and process events */
-        glfwPollEvents();
+        glfwSwapBuffers(window); // swap front and back buffers
+
+        glfwPollEvents(); // poll for and process events
     }
 
     glfwTerminate();
