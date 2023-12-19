526dc798c1ca8e49dbbfd22db3deb046a7addc59
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-12-19 18:39:32 +0530

    helrjelk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let event_loop = EventLoop::new().unwrap();
+    let window = Window::new(&event_loop).unwrap();
+    window.set_title("renegl");
+
+    event_loop.run(move)
 }
