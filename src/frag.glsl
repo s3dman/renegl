ec57937b36a30770572a0b879f52b4456fb6c206
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-12-24 01:36:50 +0530

    minor

--- src/frag.glsl
+++ src/frag.glsl
@@ -1,9 +1,9 @@
-#version 330 core
+#version 410 core
 
 out vec4 FragColor;
 uniform float iTime;
 
-void main()
+void main(){
 
-	FragColor = vec4(1.0f, 1.0f, 1.0f, sin(iTime));
+	FragColor = vec4(sin(iTime), sin(iTime*2/3), sin(iTime/3), 1.0f);
 }
